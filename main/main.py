8cb3c40d040a4279d4e869bc96f53010607a8216
Jack Detrick <98991308+JackDetrick66@users.noreply.github.com>
2025-07-01 18:33:11 -0400

    tester1

---
+++ main/main.py
@@ -0,0 +1,19 @@
+import requests
+import schedule
+import time
+
+print("We're operational...")
+def job():
+    print("Monitoring...")
+    ani_check = requests.get('https://9animetv.to/')
+    mang_check = requests.get('https://mangadex.org/')
+    print(ani_check.url)
+    print(ani_check.status_code)
+    print(mang_check.url)
+    print(mang_check.status_code)
+
+schedule.every(2).hours.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\jackd\OneDrive\Documents\GitHub\Frater_Maior"
+call .venv\Scripts\activate.bat
+python main\main.py
